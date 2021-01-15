7a61d34c2581988e9c829d9b61e9babeb667bf56
Abimbola Ojikutu <aojikutu20@aiip.alueducation.com>
2021-01-15 22:56:16 +0100

    added day 10

---
+++ Day10_Binary_Search.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sy
+
+
+def binarynumbers(n):
+    binary ="{0:b}".format(n)  # a built-in binary library in python
+    print(max(map(len,binary.split("0"))))
+    
+    
+    
+if __name__ == '__main__':
+    n = int(input())
+    binarynumbers(n)
+    
